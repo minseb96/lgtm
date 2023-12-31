5e636306ec861d0b83e515bd05c1a5bad350de03
minseb96 <minseb96@naver.com>
2023-12-31 15:04:06 +0900

    [Feature] - Add getting argument & option from command line

--- lgtm/core.py
+++ lgtm/core.py
@@ -2,11 +2,14 @@ import click
 
 
 @click.command()
-def cli():
+@click.option("--message", "-m", default="LGTM",
+              show_default=True, help="이미지에 추가할 문자열")
+@click.argument("keyword")
+def cli(keyword, message):
     """LGTM 이미지 생성 도구"""
-    lgtm()
+    lgtm(keyword, message)
     click.echo('lgtm')
 
 
-def lgtm():
+def lgtm(keyword, message):
     pass
